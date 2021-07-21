1dfff0a1959606440a804fce238e5edea5b44cb4
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-07-21 12:50:51 -0400

    working stopwatch

--- MeNRoyMobileGame/Assets/Scripts/Score.cs
+++ MeNRoyMobileGame/Assets/Scripts/Score.cs
@@ -9,12 +9,45 @@ public class Score : MonoBehaviour
     public static int scoreValue = 0;
     Text score;
     Text highestScore;
+    public int DelayAmount = 1;
+    bool stopWatchActive = false;
+    protected float timer;
+
+
+    private void Update()
+    {
+        IncreaseScoreByTime();
+    }
 
     void IncreaseScore(int Amount)
     {
         scoreValue += Amount;
-        score.text = "Score: " + scoreValue;
+        //score.text = "Score: " + scoreValue;
         highestScore.text = "Score: " + highScore;
+    }
+
+    void IncreaseScoreByTime()
+    {
+        if(stopWatchActive)
+            timer += Time.deltaTime;
+
+        if(timer >= DelayAmount)
+        {
+            timer = 0f;
+            scoreValue++;
+        }
+        score.text = "Score: " + scoreValue;
+    }
+
+    public void StartStopWatch(bool canDrag)
+    {
+        if(canDrag)
+            stopWatchActive = true;
+    }
+
+    public void StopStopWatch()
+    {
+        stopWatchActive = false;
     }
 
     private void OnEnable()
@@ -22,6 +55,7 @@ public class Score : MonoBehaviour
         score = GetComponent<Text>();
         score.text = "Score: " + scoreValue;
         GoodShape.OnScoreIncrease += IncreaseScore;
+        PlayerControl.OnDrag += StartStopWatch;
     }
 
     private void OnDisable()
