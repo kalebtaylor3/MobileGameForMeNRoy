c818fd70a053f7faaedef7c00c5ea52508471221
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-07-26 23:43:31 -0400

    I attemped the procedural generation.
    they spawn properly but i need to use your logic to get them off screen & despawning

--- MeNRoyMobileGame/Assets/Scripts/Spawner.cs
+++ MeNRoyMobileGame/Assets/Scripts/Spawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject GoodShape, BadShape, RandomShape;
+
+    private float distance;
+    private float distanceMoved;
+
+    private void Update()
     {
-        
+        if (distance < transform.position.x + 40)
+            distance = transform.position.x + 40;
+
+
+        float distToGo = Mathf.Floor(distance - distanceMoved);
+
+        if(distanceMoved < distance && distToGo > 15)
+        {
+            distanceMoved = distance;
+            SpawnEnemy();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnEnemy()
     {
-        
+        GameObject enemyToSpawn = SelectShapeToSpawn();
+
+        float yPos = Mathf.Floor(Mathf.Abs(UnityEngine.Random.Range(0, 10) - UnityEngine.Random.Range(0, 10)) * (1 + 20 - (-20)) + (-20));
+        Vector2 posToSpawnShape = new Vector2(transform.position.x + 20, transform.position.y + 20);
+
+        Instantiate(enemyToSpawn, posToSpawnShape, Quaternion.identity);
+    }
+
+    private GameObject SelectShapeToSpawn()
+    {
+        return GoodShape;
     }
 }
