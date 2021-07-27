cf243543b8db8e6283896683bb8a30d5911ea189
Aencro <roythomblison@gmail.com>
2021-07-27 01:13:23 -0400

    Generation only on -Y

---
+++ MeNRoyMobileGame/Assets/Scripts/TileManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+
+    private float spawnX = 0.0f;
+    private float spawnY = 0.0f;
+
+    private float tileLength = 20.0f;
+
+    private int tilesOnScreen = 9;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        for(int i = 0; i < tilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.y > (spawnY - tilesOnScreen * tileLength))
+        {
+            SpawnTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        go = Instantiate(tilePrefabs[0]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = transform.right * -spawnY;
+        spawnY += tileLength;
+
+
+    }
+}
