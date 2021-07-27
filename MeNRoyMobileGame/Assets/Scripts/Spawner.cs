e74fb0d0980531a57625e3fc05ed8806c8e321f8
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-07-27 00:01:31 -0400

    Revert "I attemped the procedural generation."
    This reverts commit c818fd70a053f7faaedef7c00c5ea52508471221.

--- MeNRoyMobileGame/Assets/Scripts/Spawner.cs
+++ MeNRoyMobileGame/Assets/Scripts/Spawner.cs
@@ -4,38 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject GoodShape, BadShape, RandomShape;
-
-    private float distance;
-    private float distanceMoved;
-
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (distance < transform.position.x + 40)
-            distance = transform.position.x + 40;
-
-
-        float distToGo = Mathf.Floor(distance - distanceMoved);
-
-        if(distanceMoved < distance && distToGo > 15)
-        {
-            distanceMoved = distance;
-            SpawnEnemy();
-        }
+        
     }
 
-    private void SpawnEnemy()
+    // Update is called once per frame
+    void Update()
     {
-        GameObject enemyToSpawn = SelectShapeToSpawn();
-
-        float yPos = Mathf.Floor(Mathf.Abs(UnityEngine.Random.Range(0, 10) - UnityEngine.Random.Range(0, 10)) * (1 + 20 - (-20)) + (-20));
-        Vector2 posToSpawnShape = new Vector2(transform.position.x + 20, transform.position.y + 20);
-
-        Instantiate(enemyToSpawn, posToSpawnShape, Quaternion.identity);
-    }
-
-    private GameObject SelectShapeToSpawn()
-    {
-        return GoodShape;
+        
     }
 }
