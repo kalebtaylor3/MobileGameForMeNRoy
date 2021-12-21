78bf4a17e4d2831a8e17dda77eb9adf482d1ba36
Aencro <roythomblison@gmail.com>
2021-12-20 23:50:46 -0500

    Boss Looks left and right depending on location

--- MeNRoyMobileGame/Assets/Boss.cs
+++ MeNRoyMobileGame/Assets/Boss.cs
@@ -6,6 +6,7 @@ using System;
 public class Boss : MonoBehaviour
 {
     private Transform player;
+    private Transform boss;
     private Rigidbody2D rb;
     public GameObject fireball;
 
@@ -14,6 +15,9 @@ public class Boss : MonoBehaviour
     float fireRate;
     float nextFire;
 
+    [SerializeField]
+    private SpriteRenderer mySpriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,20 +25,35 @@ public class Boss : MonoBehaviour
         fireRate = 3f;
         nextFire = Time.time;
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        boss = GameObject.FindGameObjectWithTag("Boss").transform;
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // BOSS ROTATION TOWARDS PLAYER //
-
+        /*
         Vector3 direction = player.position - transform.position;
         //Debug.Log(direction);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
-
+        */
         //            FIN             //
-
+        if (player.position.x > boss.position.x)
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+        }
+        else
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = false;
+            }
+        }
 
 
     }
