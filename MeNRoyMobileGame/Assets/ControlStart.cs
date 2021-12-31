cc3371e843139e68b374d2f9c5f6e6018d770094
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-12-31 13:32:10 -0500

    indicator text to swipe when start

--- MeNRoyMobileGame/Assets/ControlStart.cs
+++ MeNRoyMobileGame/Assets/ControlStart.cs
@@ -9,14 +9,19 @@ public class ControlStart : MonoBehaviour
     public Rigidbody2D player;
     public CanvasGroup hud;
     public UIManager ui;
+    public CanvasGroup startText;
 
     public static event Action<bool> OnTimer;
+
+    public Animator fade;
 
     // Start is called before the first frame update
     void Start()
     {
         player.constraints = RigidbodyConstraints2D.FreezePosition;
         hud.alpha = 0;
+        startText.alpha = 0;
+        fade.SetBool("fadein", true);
     }
 
     private void OnEnable()
@@ -30,6 +35,7 @@ public class ControlStart : MonoBehaviour
         ui.ShowGameHUD(true, false);
         player.constraints = RigidbodyConstraints2D.FreezeRotation;
         OnTimer?.Invoke(true);
+        fade.SetBool("fadeout", true);
         PlayerControl.OnEndDrag -= StartGame;
     }
 }
