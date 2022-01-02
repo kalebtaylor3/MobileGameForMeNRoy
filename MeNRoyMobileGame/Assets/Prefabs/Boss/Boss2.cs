cccc2daaf2b01a70036240acb8da5765d34e731a
Aencro <roythomblison@gmail.com>
2022-01-02 01:09:17 -0500

    boss looks in a circle need to make it look at player first maybe? we can discuss

---
+++ MeNRoyMobileGame/Assets/Prefabs/Boss/Boss2.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Boss2 : MonoBehaviour
+{
+    private Transform player;
+    private Transform boss;
+    private Rigidbody2D rb;
+    public GameObject fireball;
+    public GameObject bossGO;
+
+    public static event Action OnBossDeath;
+
+    float fireRate;
+    float nextFire;
+    float xHit = 0;
+
+    [SerializeField]
+    private SpriteRenderer mySpriteRenderer;
+
+    [SerializeField]
+    private Transform laser;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        fireRate = 7f;
+        nextFire = Time.time;
+        boss = GameObject.FindGameObjectWithTag("Boss").transform;
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        bossGO = GameObject.FindGameObjectWithTag("Boss");
+        StartCoroutine(WaitForScene());
+    }
+
+    IEnumerator WaitForScene()
+    {
+        yield return new WaitForSeconds(6);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        laser = laser.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // BOSS ROTATION TOWARDS PLAYER //
+
+        Vector3 direction = laser.position - transform.position;
+        //Debug.Log(direction);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        
+        //            FIN             //
+        /*
+
+        if (player.position.x > boss.position.x)
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            if (mySpriteRenderer != null)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+        }
+        */
+
+        Shoot();
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            animator.SetBool("DamageIndicator", true);
+
+            xHit++;
+            if (xHit >= 3)
+            {
+                Debug.Log(xHit);
+                Death();
+                Destroy(bossGO, 0.5f);
+            }
+            StartCoroutine(Wait2());
+        }
+
+    }
+
+    void Shoot()
+    {
+        //animator.SetBool("FireBall", true);
+        if (Time.time > nextFire)
+        {
+
+            StartCoroutine(Wait());
+            Instantiate(fireball, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+            animator.SetBool("FireBall", false);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        animator.SetBool("FireBall", true);
+    }
+    IEnumerator Wait2()
+    {
+        yield return new WaitForSeconds(0.2f);
+        animator.SetBool("DamageIndicator", false);
+    }
+
+
+
+    void Death()
+    {
+        OnBossDeath?.Invoke();
+    }
+}
---
+++ MeNRoyMobileGame/Assets/Prefabs/Boss/Laser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Laser : MonoBehaviour
+{
+    public float angularSpeed = 1f;
+    public float circleRad = 10f;
+
+    [SerializeField]
+    private Transform boss;
+
+    private Vector2 fixedpointBOSS;
+    private float currentAngle;
+
+    private bool wait;
+
+    void Start()
+    {
+        wait = false;
+        StartCoroutine(WaitForScene());
+    }
+
+    void Update()
+    {
+        if(wait)
+        {
+            currentAngle += angularSpeed * Time.deltaTime;
+            Vector2 offset = new Vector2(Mathf.Sin(currentAngle), Mathf.Cos(currentAngle)) * circleRad;
+            transform.position = fixedpointBOSS + offset;
+        }
+    }
+
+    IEnumerator WaitForScene()
+    {
+        yield return new WaitForSeconds(7);
+        fixedpointBOSS = transform.position;
+        wait = true;
+    }
+}
